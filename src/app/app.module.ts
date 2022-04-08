d38548d86817b877aa5b93d7a328ab8789daebc9
uh! <76551996+clqu@users.noreply.github.com>
2022-04-08 07:13:27 +0300

    Added error if router is not found.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ const ModuleArray = [];
 fs.readdirSync(path.join(__dirname, '../routers/')).forEach((folders) => {
   const _folders = fs.readdirSync('./src/routers/' + folders + '/');
   _folders.filter(a => a.includes('module')).forEach(mfile => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { RouterModule } = require(path.join(__dirname, '../routers/' + folders + '/' + mfile.split('.ts')[0]+'.js'));
-    ModuleArray.push(RouterModule)
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { RouterModule } = require(path.join(__dirname, '../routers/' + folders + '/' + mfile.split('.ts')[0]+'.js'));
+      if(!RouterModule) throw new Error(`RouterModule not defined.`)
+      ModuleArray.push(RouterModule);
+    } catch(error) {
+      throw new Error(`[routers${folders}/${mfile}]: An error occurred while loading.\n${error?.message}\n\n`)
+    }
+
   });
 });
 @Module({
